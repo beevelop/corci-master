afc77471f97d704487ca3814548a1e3dd6ddcc2b
Maik Hummel <yo@beevelop.com>
2015-07-17 23:22:20 +0200

    add grunt-bump for uniformed releases

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,11 +11,27 @@ module.exports = function (grunt) {
                     destination: 'docs'
                 }
             }
+        },
+        bump: {
+            options: {
+                files: ['package.json'],
+                updateConfigs: [],
+                commit: true,
+                commitMessage: 'Release v%VERSION%',
+                commitFiles: ['-a'],
+                createTag: true,
+                tagName: '%VERSION%',
+                tagMessage: 'Version %VERSION%',
+                push: true,
+                pushTo: 'origin',
+                gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d'
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-jsdoc');
+    grunt.loadNpmTasks('grunt-bump');
 
     grunt.registerTask('default', 'jshint');
     grunt.registerTask('docs', 'jsdoc');
